0d2a505ee057f9c416b79eaa937032f0a737cf4b
Timofey Asyrkin <tasyrkin@gmail.com>
2015-11-24 22:08:07 -0800

    Added error reporting and enhanced README.md

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ To launch the `load_page.js` script run
 ```
 phantomjs load_page.js http://www.google.com
 ```
+
+[Good resource](https://newspaint.wordpress.com/2013/04/25/getting-to-the-bottom-of-why-a-phantomjs-page-load-fails/) for explanation of how to use `var page = require('webpage').create()` object.
--- load_page.js
+++ load_page.js
@@ -9,10 +9,20 @@ if (system.args.length === 1){
 }
 page_address = system.args[1]
 
+page.onResourceError = function(resourseError){
+  page.reason = resourseError.errorString;
+  page.reason_url = resourseError.url;
+};
+
 page.open(page_address, function(status){
-    console.log('Status:' + status);
     if (status === 'success' ){
-      fs.write('load_page_result.html', page.content, 'w')
+      var destination_file = 'load_page_result.html';
+      console.log('Successfully loaded url [' + page_address + ']');
+      fs.write(destination_file, page.content, 'w')
+      console.log('Successfully written content into [' + destination_file + ']');
+      phantom.exit( 0 );
+    } else {
+      console.log( 'Error opening url [' + page.reason_url + '], reason: [' + page.reason + ']' );
+      phantom.exit( 1 );
     }
-    phantom.exit();
 });
